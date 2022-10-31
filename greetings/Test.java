ae1b639c6fabb9c63eef74f385f487a3dd735d09
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-10-31 00:21:45 -0400

    first commit

---
+++ greetings/Test.java
@@ -0,0 +1,16 @@
+//File name must be the same as the class name
+//Every file IS a class
+
+public class Test{
+
+    //The file or class within the exact method signature below
+    //is always teh launching point of the application
+    //You cannot run a java program without it
+
+    public static void main(String[] args){
+
+//For now all your code goes here!
+
+        System.out.println("Hello World!");
+    }
+}
