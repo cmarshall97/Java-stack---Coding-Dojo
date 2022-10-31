e9db41f66dd829a38e66b07a46704811e25af0c9
Carol Marshall <104531693+Carol-M-Marshall@users.noreply.github.com>
2022-10-31 13:20:43 -0400

    specifying the number of weeks

---
+++ Cafe_Business_Logic/CafeUtil.java
@@ -0,0 +1,12 @@
+//ALL methods in CafeUtil class
+
+class CafeUtil {
+
+    public int getStreakGoal(){
+        int sum = 0;
+        for (int i =1; i<=10; i++){
+            sum = sum + i ;
+        }
+        return sum;
+    }
+}
